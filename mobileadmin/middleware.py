5cc88cf1e73fc32842e11f0bee5c6c9fefc27bc4
iusgentium <iusgentium@4ed3a04f-053e-0410-b24d-b35526dee1fc>
2007-12-19 13:27:17 +0000

    Added BlackBerry & Opera mini browsers to middleware.py. Updated docstrings in middleware.py. Added main_readable.css, replaced main.css with reducred size (12KB -> 8KB).
    git-svn-id: https://django-mobileadmin.googlecode.com/svn/trunk@10 4ed3a04f-053e-0410-b24d-b35526dee1fc

--- mobileadmin/middleware.py
+++ mobileadmin/middleware.py
@@ -1,3 +1,10 @@
+"""
+Middleware sets a flag, when a mobile device is requesting the site.
+
+This is used later on to load special mobile-admin templates.
+
+"""
+
 import os, re
 from django.core.urlresolvers import reverse, NoReverseMatch
 from django.conf import settings
@@ -10,11 +17,13 @@ except ImportError:
 _thread_locals = local()
 
 safari_regex = re.compile(r'AppleWebKit/.*Mobile/')
+blackbarry_regex = re.compile(r'^BlackBerry')
+opera_mini_regex = re.compile(r'[Oo]pera [Mm]ini')
 
 def normalize_slashes(path):
     """
-    Removes leading, trailing and multiple slashes from a given path
-    
+    Removes leading, trailing and multiple slashes from a given path.
+
     >>> path = "/asd/ad/"
     >>> normalize_slashes(path)
     'asd/ad'
@@ -26,23 +35,32 @@ def normalize_slashes(path):
 
 def get_thread_var(var_name):
     """
-    Loads variable from the local thread
+    Loads variable from the local thread.
     """
     return getattr(_thread_locals, var_name, None)
 
 def set_thread_var(var_name, value):
     """
-    Sets variable in the local thread   
+    Sets variable in the local thread.
     """
     setattr(_thread_locals, var_name, value)
 
 class MobileAdminMiddleware:
     """
-    Checks for the iPhone/iPod touch user agent string if the admin interface
-    is requested. Sets a thread var which is later used in a custom template
-    loader to decide which template to load.
-    
-    Adds the User-Agent field to the Vary header to circumvent false caching.
+    Checks for the some mobile device user agent strings, when the
+    admin interface is requested. Sets a thread var which is later used
+    in a custom template loader to decide which template to load.
+
+    Adds the User-Agent field to the Vary header to circumvent false
+    caching.
+
+    These browsers are supported:
+
+        * Safari (iPhone / iPod touch)
+        * BlackBerry's internal browser
+        * `Opera mini`_
+
+    .. _Opera mini: http://www.operamini.com/demo/
     """
     def process_request(self, request):
         user_agent = request.META.get('HTTP_USER_AGENT', '')
@@ -52,8 +70,10 @@ class MobileAdminMiddleware:
             except NoReverseMatch:
                 admin_url = None
             if admin_url is not None and \
-                safari_regex.search(user_agent) is not None and \
-                    normalize_slashes(request.path).startswith(admin_url):
+                (safari_regex.search(user_agent) is not None or \
+                     opera_mini_regex.search(user_agent) is not None or \
+                     blackbarry_regex.search(user_agent) is not None ) and \
+                normalize_slashes(request.path).startswith(admin_url):
                 set_thread_var('use_mobile_templates', True)
             else:
                 set_thread_var('use_mobile_templates', False)
