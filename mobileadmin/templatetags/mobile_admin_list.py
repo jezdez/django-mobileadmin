78284d87c130b0e4e10f51df08e2971a03004faa
leidel <leidel@4ed3a04f-053e-0410-b24d-b35526dee1fc>
2008-04-15 20:53:33 +0000

    changed paginator template tag to follow new paginator class, thanks paul bissex
    git-svn-id: https://django-mobileadmin.googlecode.com/svn/trunk@16 4ed3a04f-053e-0410-b24d-b35526dee1fc

--- mobileadmin/templatetags/mobile_admin_list.py
+++ mobileadmin/templatetags/mobile_admin_list.py
@@ -22,8 +22,8 @@ def pagination(cl):
 
         # If there are 4 or fewer pages, display links to every page.
         # Otherwise, do some fancy
-        if paginator.pages <= 3:
-            page_range = range(paginator.pages)
+        if paginator.num_pages <= 3:
+            page_range = range(paginator.num_pages)
         else:
             # Insert "smart" pagination links, so that there are always ON_ENDS
             # links at either end of the list of pages, and there are always
@@ -34,11 +34,11 @@ def pagination(cl):
                 page_range.extend(range(page_num - ON_EACH_SIDE, page_num + 1))
             else:
                 page_range.extend(range(0, page_num + 1))
-            if page_num < (paginator.pages - ON_EACH_SIDE - 1):
+            if page_num < (paginator.num_pages - ON_EACH_SIDE - 1):
                 page_range.extend(range(page_num + 1, page_num + ON_EACH_SIDE + 1))
-                page_range.extend(range(paginator.pages, paginator.pages))
+                page_range.extend(range(paginator.num_pages, paginator.num_pages))
             else:
-                page_range.extend(range(page_num + 1, paginator.pages))
+                page_range.extend(range(page_num + 1, paginator.num_pages))
 
     need_show_all_link = cl.can_show_all and not cl.show_all and cl.multi_page
     return {
