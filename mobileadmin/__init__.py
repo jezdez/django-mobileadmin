eac647efc9422c3eeafb8ba62452a4851faf4825
leidel <leidel@4ed3a04f-053e-0410-b24d-b35526dee1fc>
2009-01-03 11:31:29 +0000

    fixed bug in autoregister where attribute searching didn't search deep enough (didn't search base classes)
    Signed-off-by: Jannis Leidel <jannis@leidel.info>
    
    git-svn-id: https://django-mobileadmin.googlecode.com/svn/trunk@49 4ed3a04f-053e-0410-b24d-b35526dee1fc

--- mobileadmin/__init__.py
+++ mobileadmin/__init__.py
@@ -4,6 +4,21 @@ from django.core.exceptions import ImproperlyConfigured
 from mobileadmin import decorators, views
 from mobileadmin.conf import settings
 
+### From http://www2.lib.uchicago.edu/keith/courses/python/class/5/#attrref
+def classlookup(C, name):
+	if C.__dict__.has_key(name):
+		return (1, C.__dict__[name])
+	else:
+		for b in C.__bases__:
+		    success, value = classlookup(b, name)
+		    if success:
+			    return (1, value)
+		    else:
+			    pass
+		else:
+		    return (0, None)
+
+
 def autoregister():
     """
     Auto-register all ModelAdmin instances of the default AdminSite with the
@@ -14,11 +29,11 @@ def autoregister():
     
     for model, modeladmin in main_site._registry.iteritems():
         admin_class = modeladmin.__class__
-
-        for name, value in admin_class.__dict__.iteritems():
-            if name in settings.TEMPLATE_MAPPING:
+        for name in settings.TEMPLATE_MAPPING:
+            (found, value) = classlookup(admin_class, name)
+            if found:
                 setattr(admin_class, name, decorators.mobile_templates(value))
-
+                
         if admin_class == UserAdmin:
             setattr(admin_class, 'add_view', views.auth_add_view)
 
